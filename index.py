0d9c6415e647e654bb8ea67bbed7e2c62b35bfe6
soapmactavish23 <ricknogueira1231@gmail.com>
2024-04-24 21:57:16 -0300

    ajuestes nas imagens

--- index.py
+++ index.py
@@ -2,7 +2,7 @@
 import numpy as np
 import os
 import cv2
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+#from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from tensorflow.keras.optimizers import Adam
